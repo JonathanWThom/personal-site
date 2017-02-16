8b7ba3d6086811735b03ff96f053ee0a71958b90
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-15 22:03:31 -0800

    Will add post title to route

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,19 @@
 class Post < ActiveRecord::Base
   validates :title, :content, :date, :presence => true
+
+  # validates_format_of :title, :without => /^\d/
+
+  def to_param
+    title
+  end
+
+  def self.find(input)
+    input.to_i == 0 ? find_by(title: input) : super
+  end
+
+
+
+  # def self.find_by_param(input)
+  #   find_by(title: input)
+  # end
 end
