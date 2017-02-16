56dbea970e98100ea463d98adf20d831ef78fcaa
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-15 22:11:29 -0800

    Add head to post detail page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, :content, :date, :presence => true
 
-  # validates_format_of :title, :without => /^\d/
-
   def to_param
     title
   end
@@ -11,9 +9,4 @@ class Post < ActiveRecord::Base
     input.to_i == 0 ? find_by(title: input) : super
   end
 
-
-
-  # def self.find_by_param(input)
-  #   find_by(title: input)
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
-  resources :posts #as blog
+  resources :posts, :path => 'blog'
 end
