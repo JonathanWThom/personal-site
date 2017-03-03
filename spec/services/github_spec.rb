edb704f1031c8ed70fa2ae2b1f7e1a51aedb0e90
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-02 21:49:50 -0800

    testing

--- spec/services/github_spec.rb
+++ spec/services/github_spec.rb
@@ -10,4 +10,8 @@ describe Github do
     github = Github.new
     expect(github.recently_updated.length).to eq(5)
   end
+
+  it 'will fail the api call', vcr: true do
+    ## not quite sure how to do this since my call doesn't take parameters
+  end
 end
