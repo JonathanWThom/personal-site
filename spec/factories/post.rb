90a808db3d27a41e4b664af3736dfd6b20272836
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-02 20:57:06 -0800

    Specs

---
+++ spec/factories/post.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :post do
+    title "Test Post"
+    content  "Lorem Ipsum"
+    date '3/2/17'
+  end
+end
---
+++ spec/features/admin_path_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'the admin path' do
+  it 'will log in an admin' do
+    create(:admin)
+    visit new_admin_session_path
+    fill_in 'admin_email', :with => 'admin@admin.com'
+    fill_in 'admin_password', :with => 'password'
+    click_on 'Login'
+    expect(page).to have_content 'Dashboard'
+  end
+
+  it 'will create a new admin' do
+    admin = create(:admin)
+    login_as(admin)
+    visit new_admin_admin_path
+    fill_in 'Email', :with => 'admin2@admin.com'
+    fill_in 'admin_password', :with => 'password'
+    fill_in 'admin_password_confirmation', :with => 'password'
+    click_on 'Create Admin'
+    expect(page).to have_content 'Admin was successfully created.'
+  end
+
+  it 'will create a project' do
+    admin = create(:admin)
+    login_as(admin)
+    visit new_admin_project_path
+    fill_in 'project_title', :with => 'admin2@admin.com'
+    fill_in 'project_description', :with => 'description'
+    fill_in 'project_collaborators', :with => 'collaborators'
+    fill_in 'project_github', :with => 'github'
+    fill_in 'project_deployed', :with => 'deployed'
+    fill_in 'project_tech', :with => 'tech'
+    attach_file "project_image", "spec/asset_specs/photos/leaf.jpeg"
+    click_on 'Create Project'
+    expect(page).to have_content 'Project was successfully created.'
+  end
+end
