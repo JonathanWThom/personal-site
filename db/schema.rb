447b148d57ba708cf77ab76c6617c66982d72f63
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-03 13:50:28 -0800

    Change post text field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226040018) do
+ActiveRecord::Schema.define(version: 20170303214830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20170226040018) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content"
     t.string   "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
