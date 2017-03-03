6c96eebce0cb5c49bbc5e4d6ca20945d4cd06da3
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-03 09:41:25 -0800

    Refactor api requests

--- app/services/github.rb
+++ app/services/github.rb
@@ -1,7 +1,7 @@
 class Github
   def top_starred
     begin
-      results = RestClient.get("https://api.github.com/users/jonathanwthom/repos?per_page=100", headers={"authorization": ENV['GITHUB_ACCESS_TOKEN'] })
+      results = RestClient::Request.execute(method: :get, url: 'https://api.github.com/users/jonathanwthom/repos?per_page=100', headers: {Authorization: ENV['GITHUB_ACCESS_TOKEN']})
       stars = JSON.parse(results).sort_by { |k| k["stargazers_count"] }
       stars.last(5).reverse
     rescue RestClient::ExceptionWithResponse => e
@@ -12,7 +12,7 @@ class Github
 
   def recently_updated
     begin
-      results = RestClient.get("https://api.github.com/users/jonathanwthom/repos?per_page=100", headers={"authorization": ENV['GITHUB_ACCESS_TOKEN'] })
+      results = RestClient::Request.execute(method: :get, url: 'https://api.github.com/users/jonathanwthom/repos?per_page=100', headers: {Authorization: ENV['GITHUB_ACCESS_TOKEN']})
       recent = JSON.parse(results).sort_by { |k| k["updated_at"] }
       recent.last(5).reverse
     rescue RestClient::ExceptionWithResponse => e
@@ -23,7 +23,7 @@ class Github
 
   def profile_pic
     begin
-      results = RestClient.get("https://api.github.com/users/jonathanwthom", headers={"authorization": ENV['GITHUB_ACCESS_TOKEN'] })
+      results = RestClient::Request.execute(method: :get, url: "https://api.github.com/users/jonathanwthom", headers: {Authorization: ENV['GITHUB_ACCESS_TOKEN']})
       profile_pic = JSON.parse(results)['avatar_url']
     rescue RestClient::ExceptionWithResponse => e
       e.response
