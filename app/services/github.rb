404a24cecda81ef4001a408c355ad19252d59536
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-02 19:32:45 -0800

    Take care of error conditions for api and in views

--- app/services/github.rb
+++ app/services/github.rb
@@ -1,13 +1,21 @@
 class Github
   def top_starred
-    results = RestClient.get("https://api.github.com/users/jonathanwthom/repos?per_page=100", headers: {"authorization": ENV['GITHUB_ACCESS_TOKEN'] })
-    stars = JSON.parse(results).sort_by { |k| k["stargazers_count"] }
-    stars.last(5).reverse
+    begin
+      results = RestClient.get("https://api.github.com/users/jonathanwthom/repos?per_page=100", headers: {"authorization": ENV['GITHUB_ACCESS_TOKEN'] })
+      stars = JSON.parse(results).sort_by { |k| k["stargazers_count"] }
+      stars.last(5).reverse
+    rescue RestClient::ExceptionWithResponse => e
+      e.response['message']
+    end
   end
 
   def recently_updated
-    results = RestClient.get("https://api.github.com/users/jonathanwthom/repos?per_page=100", headers: {"authorization": ENV['GITHUB_ACCESS_TOKEN'] })
-    recent = JSON.parse(results).sort_by { |k| k["updated_at"] }
-    recent.last(5).reverse
+    begin
+      results = RestClient.get("https://api.github.com/users/jonathanwthom/repos?per_page=100", headers: {"authorization": ENV['GITHUB_ACCESS_TOKEN'] })
+      recent = JSON.parse(results).sort_by { |k| k["updated_at"] }
+      recent.last(5).reverse
+    rescue RestClient::ExceptionWithResponse => e
+      e.response['message']
+    end
   end
 end
