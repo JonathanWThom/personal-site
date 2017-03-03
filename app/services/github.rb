00bcf437266ae5549fdc0d58c273cb8527e1aa26
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-03 08:59:57 -0800

    Update gitignore

--- app/services/github.rb
+++ app/services/github.rb
@@ -1,30 +1,33 @@
 class Github
   def top_starred
     begin
-      results = RestClient.get("https://api.github.com/users/jonathanwthom/repos?per_page=100", headers: {"authorization": ENV['GITHUB_ACCESS_TOKEN'] })
+      results = RestClient.get("https://api.github.com/users/jonathanwthom/repos?per_page=100", headers={"authorization": ENV['GITHUB_ACCESS_TOKEN'] })
       stars = JSON.parse(results).sort_by { |k| k["stargazers_count"] }
       stars.last(5).reverse
     rescue RestClient::ExceptionWithResponse => e
-      e.response['message']
+      e.response
+      puts e.response
     end
   end
 
   def recently_updated
     begin
-      results = RestClient.get("https://api.github.com/users/jonathanwthom/repos?per_page=100", headers: {"authorization": ENV['GITHUB_ACCESS_TOKEN'] })
+      results = RestClient.get("https://api.github.com/users/jonathanwthom/repos?per_page=100", headers={"authorization": ENV['GITHUB_ACCESS_TOKEN'] })
       recent = JSON.parse(results).sort_by { |k| k["updated_at"] }
       recent.last(5).reverse
     rescue RestClient::ExceptionWithResponse => e
-      e.response['message']
+      e.response
+      puts e.response
     end
   end
 
   def profile_pic
     begin
-      results = RestClient.get("https://api.github.com/users/jonathanwthom", headers: {"authorization": ENV['GITHUB_ACCESS_TOKEN'] })
+      results = RestClient.get("https://api.github.com/users/jonathanwthom", headers={"authorization": ENV['GITHUB_ACCESS_TOKEN'] })
       profile_pic = JSON.parse(results)['avatar_url']
     rescue RestClient::ExceptionWithResponse => e
-      e.response['message']
+      e.response
+      puts e.response
     end
   end
 end
