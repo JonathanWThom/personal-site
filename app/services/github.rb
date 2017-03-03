42487063aec477947409969e774a3a8931c24ff7
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-03 13:42:11 -0800

    Refactor api call

--- app/services/github.rb
+++ app/services/github.rb
@@ -1,9 +1,8 @@
 class Github
   def top_starred
     begin
-      results = RestClient::Request.execute(method: :get, url: 'https://api.github.com/users/jonathanwthom/repos?per_page=100', headers: {Authorization: ENV['GITHUB_ACCESS_TOKEN']})
-      stars = JSON.parse(results).sort_by { |k| k["stargazers_count"] }
-      stars.last(5).reverse
+      results = RestClient::Request.execute(method: :get, url: 'https://api.github.com/search/repositories?q=user:jonathanwthom&sort=stars&order=desc&per_page=100', headers: {Authorization: ENV['GITHUB_ACCESS_TOKEN']})
+      JSON.parse(results)['items'].first(5)
     rescue RestClient::ExceptionWithResponse => e
       e.response
       puts e.response
@@ -12,9 +11,8 @@ class Github
 
   def recently_updated
     begin
-      results = RestClient::Request.execute(method: :get, url: 'https://api.github.com/users/jonathanwthom/repos?per_page=100', headers: {Authorization: ENV['GITHUB_ACCESS_TOKEN']})
-      recent = JSON.parse(results).sort_by { |k| k["updated_at"] }
-      recent.last(5).reverse
+      results = RestClient::Request.execute(method: :get, url: 'https://api.github.com/search/repositories?q=user:jonathanwthom&sort=updated', headers: {Authorization: ENV['GITHUB_ACCESS_TOKEN']})
+      JSON.parse(results)['items'].first(5)
     rescue RestClient::ExceptionWithResponse => e
       e.response
       puts e.response
