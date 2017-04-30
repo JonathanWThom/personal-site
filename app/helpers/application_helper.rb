85a087219489d7d84fc1b9a2d721cd2e3f5afbe9
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-04-29 19:54:44 -0700

    Add simple format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   FORMAT = AutoHtml::Pipeline.new(
       AutoHtml::Markdown.new,
-      AutoHtml::Link.new
+      AutoHtml::Link.new,
+      AutoHtml::SimpleFormat.new
     )
 end
