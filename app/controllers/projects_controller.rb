9a523a002a7de99c1f5f2a865702b008791dfdd0
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-05-24 21:31:03 -0700

    Change order of projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def index
-    @featured = Project.all
+    @featured = Project.order("created_at desc")
     github = Github.new('jonathanwthom')
     @recent = github.recently_updated
     @top_starred = github.top_starred
