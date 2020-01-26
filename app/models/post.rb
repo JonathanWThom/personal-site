afd6dccab513d61abb1a6e45361bb049aceee162
Jonathan Thom <jonathan.thom1990@gmail.com>
2020-01-25 19:23:49 -0800

    Improve post titles

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,12 @@ class Post < ActiveRecord::Base
   validates :title, :content, :date, :presence => true
 
   def to_param
-    title
+    title.gsub(" ", "-")
   end
 
   def self.find(input)
+    # this won't always work, better to use a save slug in the future
+    input = input.gsub("-", " ")
     input.to_i == 0 ? find_by(title: input) : super
   end
 
