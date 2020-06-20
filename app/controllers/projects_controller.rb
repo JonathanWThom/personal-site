f734b3abc464767f12e949fe2790147b23a53072
Jonathan Thom <jonathanthom@hey.com>
2020-06-20 12:38:24 -0700

    Remove projects query

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,5 @@
 class ProjectsController < ApplicationController
   def index
-    @featured = Project.order("created_at desc")
     github = Github.new('jonathanwthom')
     @recent = github.recently_updated
     @top_starred = github.top_starred
