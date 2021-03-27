d77fce105ec9372c8996b09f63bc968adca69dfe
Jonathan Thom <jonathanthom@hey.com>
2021-03-26 23:31:28 -0700

    Remove api call in projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,5 @@ class ProjectsController < ApplicationController
   def index
     github = Github.new('jonathanwthom')
     @recent = github.recently_updated
-    @top_starred = github.top_starred
   end
 end
